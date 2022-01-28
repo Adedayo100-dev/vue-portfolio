b5e2256d59c2ee1a57f8d460d93bd0e8c4dc6fee
Taiwo Adedayo {{ Dee - Why }} <66147046+Adedayo100-ux@users.noreply.github.com>
2022-01-28 02:14:22 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <Navbar/>
+  </div>
+  <Sidebar/>
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+export default {
+  components: {
+    Navbar,
+    Sidebar
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/style.css'
+import './assets/css/light-theme.css'
+import './assets/css/hover button.css'
+import './assets/css/about.css'
+import './assets/css/contact.css'
+import './assets/css/floating-labels.css'
+import './assets/css/pricing.css'
+import './assets/css/works.css'
+
+createApp(App).use(router).mount('#app')
